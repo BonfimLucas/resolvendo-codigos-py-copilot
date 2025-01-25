f379d4dab74f87c05290494eaee2fecebf9aeeeb
BonfimLucas <98068457+BonfimLucas@users.noreply.github.com>
2025-01-25 12:28:45 +0000

    add python code

--- resolucoes_code/concat_dados.py
+++ resolucoes_code/concat_dados.py
@@ -1,1 +1,6 @@
 # Vamos receber dois dados diferentes do usuário e concatena-los em uma única string?!
+
+palavra1 = input("Digite uma palavra: ")
+palavra2 = input("Digite outra palavra: ")
+
+print(f"{palavra1} {palavra2}")
--- resolucoes_code/ope_mat.py
+++ resolucoes_code/ope_mat.py
@@ -1,1 +1,6 @@
 # Vamos solicitar como entrada dois números e depois vamos realizar uma operação simples entre eles.
+
+num1 = input("Digite um numero: ")
+num2 = input("Digite outro numero: ")
+
+print(int(num1) + int(num2))
